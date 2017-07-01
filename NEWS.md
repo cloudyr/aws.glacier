f1b73b7bdedb48284ec7d6e2b9baf86050cc46e1
Thomas J. Leeper <thosjleeper@gmail.com>
2017-07-01 08:45:02 +0100

    bump aws.signature dependency

---
+++ NEWS.md
@@ -0,0 +1,7 @@
+# CHANGES TO aws.glacier 0.1.6
+
+* Bumped **aws.signature** dependency to v0.3.4.
+
+# CHANGES TO aws.glacier 0.1.1
+
+* Initial release.
--- R/http.r
+++ R/http.r
@@ -9,51 +9,52 @@
 #' @param query A list of HTTP URL query parameters.
 #' @param headers A list of HTTP headers.
 #' @param body The body of the request.
-#' @param region A character string containing the AWS region.
-#' If missing, defaults to \dQuote{us-east-1}.
-#' @param key A character string containing an AWS Access Key ID. 
-#' If missing, defaults to value stored in environment variable \dQuote{AWS_ACCESS_KEY_ID}.
-#' @param secret A character string containing an AWS Secret Access Key. 
-#' If missing, defaults to value stored in environment variable \dQuote{AWS_SECRET_ACCESS_KEY}.
+#' @param region A character string containing the AWS region. If missing, defaults to \dQuote{us-east-1}.
+#' @param key A character string containing an AWS Access Key ID. See \code{\link[aws.signature]{locate_credentials}}.
+#' @param secret A character string containing an AWS Secret Access Key. See \code{\link[aws.signature]{locate_credentials}}.
+#' @param session_token A character string containing an AWS Session Token. See \code{\link[aws.signature]{locate_credentials}}.
 #' @param \dots Additional arguments passed to \code{\link[httr]{GET}}, etc.
 #' @return A list.
 #' @import httr
 #' @importFrom aws.signature signature_v4_auth
 #' @importFrom XML xmlToList xmlParse
 #' @export
-glacierHTTP <- function(verb, action, query = list(), headers = list(), body = "", 
-                        region = Sys.getenv("AWS_DEFAULT_REGION","us-east-1"), 
-                        key = Sys.getenv("AWS_ACCESS_KEY_ID"), 
-                        secret = Sys.getenv("AWS_SECRET_ACCESS_KEY"), 
-                        ...) {
+glacierHTTP <- 
+function(verb, action, query = list(), headers = list(), body = "", 
+         region = Sys.getenv("AWS_DEFAULT_REGION", "us-east-1"), 
+         key = NULL, 
+         secret = NULL, 
+         session_token = NULL,
+         ...) {
     if (missing(verb)) {
         verb <- "GET"
     }
     current <- Sys.time()
     d_timestamp <- format(current, "%Y%m%dT%H%M%SZ", tz = "UTC")
     url <- paste0("https://glacier.", region, ".amazonaws.com", action)
-    if (key == "") {
-        stop("Must supply AWS AccessKeyId and SecretAccessKey")
-    } else {
-        headers$host <- paste0("glacier.",region,".amazonaws.com")
-        headers$`x-amz-date` <- d_timestamp
-        if (body != "") {
-            headers$`x-amz-sha256-tree-hash` <- .treehash(body)
-        }
-        S <- signature_v4_auth(
-               datetime = d_timestamp,
-               region = region,
-               service = "glacier",
-               verb = verb,
-               action = action,
-               query_args = query,
-               canonical_headers = headers,
-               request_body = body,
-               key = key, secret = secret)
-        headers$`x-amz-content-sha256` <- S$BodyHash
-        headers$Authorization <- S$SignatureHeader
-        H <- do.call("add_headers", headers)
+    headers$host <- paste0("glacier.",region,".amazonaws.com")
+    headers$`x-amz-date` <- d_timestamp
+    if (body != "") {
+        headers$`x-amz-sha256-tree-hash` <- .treehash(body)
     }
+    S <- signature_v4_auth(
+           datetime = d_timestamp,
+           region = region,
+           service = "glacier",
+           verb = verb,
+           action = action,
+           query_args = query,
+           canonical_headers = headers,
+           request_body = body,
+           key = key, 
+           secret = secret,
+           session_token)
+    headers$`x-amz-content-sha256` <- S$BodyHash
+    headers$Authorization <- S$SignatureHeader
+    if (!is.null(session_token) && session_token != "") {
+        headers[["x-amz-security-token"]] <- session_token
+    }
+    H <- do.call("add_headers", headers)
     if (verb == "GET") {
         if (length(query)) {
             r <- GET(url, H, query = query, ...)
@@ -89,18 +90,15 @@ glacierHTTP <- function(verb, action, query = list(), headers = list(), body = "
         }
     }
     
-    x <- xmlToList(xmlParse(content(r, "text")))
-    if (http_status(r)$category == "client error") {
+    if (http_error(r)) {
         warn_for_status(r)
         h <- headers(r)
-        out <- structure(x, headers = h, class = "aws_error")
-    } else {
-        out <- x
-    }
-    if (inherits(out, "aws_error")) {
+        out <- structure(content(r, "text", encoding = "UTF-8"), headers = h, class = "aws_error")
         attr(out, "request_canonical") <- S$CanonicalRequest
         attr(out, "request_string_to_sign") <- S$StringToSign
         attr(out, "request_signature") <- S$SignatureHeader
+    } else {
+        out <- xmlToList(xmlParse(content(r, "text")))
     }
     return(out)
 }
